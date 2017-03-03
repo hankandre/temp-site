f35df49db239d876ed14938d4043e9d6ed8d49e4
Hank Andre <hank.andre@irishtitan.com>
2017-03-03 15:40:54 -0600

    hid toast and then showed it rather than the other way around

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 'use strict'
 
-
 window.onload = function () {
+  let toast = document.getElementById('toast')
+  
   setTimeout(function() {
-    let toast = document.getElementById('toast')
-    toast.classList.remove('show')
-  }, 6000);
+    toast.classList.add('show')
+    return setTimeout(function() {
+      toast.classList.remove('show')
+    }, 6000);
+  }, 500);
 }

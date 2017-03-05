e973e7e645c0775d8c34bf78e5610491286ead38
Hank Andre <hank.andre@irishtitan.com>
2017-03-04 23:06:21 -0600

    finished sidebar styling

--- scripts.js
+++ scripts.js
@@ -1,60 +1,87 @@
 'use strict';
 
-(function (window) {
-  window.onload = function () {
-    
-    let toast = {
-      element: document.getElementById('toast'),
-      init: function init () {
-        setTimeout(function () {
-          toast.element.classList.add('show')
-          return setTimeout(function () {
-            toast.element.classList.remove('show')
-          }, 6000);
-        }, 500);
-      }
-    }
-    
-    toast.init()
+window.onload = function () {
 
-    let scroll = {
-      elements: document.getElementsByClassName('scroll-to'),
-      scrollTo: function scrollTo(event) {
-        let element = event.target
-        let location = element.getAttribute('data')
-        let targetElement = document.getElementById(location)
-        let elemRect = targetElement.getBoundingClientRect();
-        
 
-        return window.scroll(0, elemRect.top)
-      },
-      toTop: function toTop () {
-        let element = document.getElementById('toTop')
-        element.addEventListener('click',function () {
-          return window.scroll(0, 0)
-        })
-      },
-      init: function init () {
-        scroll.toTop()
-        for (let i = 0; i < scroll.elements.length; i++) {
-          let element = scroll.elements[i];
-          element.addEventListener('click', scroll.scrollTo)
-        }
-      }
+  
+  let toast = {
+    element: document.getElementById('toast'),
+    init: function init () {
+      return setTimeout(function () {
+        toast.element.classList.add('show')
+        return setTimeout(function () {
+          toast.element.classList.remove('show')
+        }, 6000);
+      }, 500);
     }
-    scroll.init()
-
-    let openSidebar = {
-      toggle: function toggle (event) {
-        let sidebar = document.getElementById('sidebar')
-        return sidebar.classList.length === 0 ? sidebar.classList.add('open') : sidebar.classList.remove('open')
-      },
-      init: function init() {
-        let button = document.getElementById('menuButton')
-        return button.addEventListener('click', openSidebar.toggle)
-      }
-    }
-    openSidebar.init()
   }
-})(window)
+  
+  toast.init()
+
+  let scroll = {
+    elements: document.getElementsByClassName('scroll-to'),
+    scrollTo: function scrollTo(event) {
+      let sidebar = document.getElementById('sidebar')
+      let menuButton = document.getElementById('menuButton')
+      let element = event.target
+      let location = element.getAttribute('data');
+      let targetElement = document.getElementById(location);
+      let elemPosition = targetElement.getBoundingClientRect().top
+      let offset = window.scrollY + elemPosition;
+
+      elemPosition ===  0 ? null : window.scroll({
+        top: offset,
+        behavior: 'smooth'
+      })
+      sidebar.classList.remove('open')
+      clearTimeout(window.timer)
+      return menuButton.classList.remove('open')
+    },
+    toTop: function toTop () {
+      let element = document.getElementById('toTop')
+      window.onscroll = function () {
+        return window.scrollY < 150 ? element.style.opacity = 0 : element.style.opacity = '.75'
+      }
+      return element.addEventListener('click',function () {
+        return window.scroll({
+          top: 0,
+          behavior: 'smooth'
+        })
+      })
+    },
+    init: function init () {
+      scroll.toTop()
+      for (let i = 0; i < scroll.elements.length; i++) {
+        let element = scroll.elements[i];
+        element.addEventListener('click', scroll.scrollTo)
+      }
+    }
+  }
+  scroll.init()
+
+  let openSidebar = {
+    toggle: function toggle (event) {
+      let sidebar = document.getElementById('sidebar')
+      let menuButoon = document.getElementById('menuButton')
+
+      if (sidebar.classList.length === 0) {
+        sidebar.classList.add('open')
+        menuButton.classList.add('open')
+        window.timer = setTimeout(function () {
+          sidebar.classList.remove('open')
+          menuButton.classList.remove('open')
+        }, 3000)
+      } else {
+        clearTimeout(window.timer)
+        menuButoon.classList.remove('open')
+        return sidebar.classList.remove('open')
+      }
+    },
+    init: function init() {
+      let button = document.getElementById('menuButton')
+      return button.addEventListener('click', openSidebar.toggle)
+    }
+  }
+  openSidebar.init()
+}
 
